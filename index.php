d6a4353b3c03c444463fb3c2f6d893c8f8992da6
Jody Moore <jodymoore@g.harvard.edu>
2017-02-21 12:52:29 -0600

    clean up number format4

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
     <title>Bill Spitter</title>
     <link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' rel='stylesheet'>
     <link href='https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/flatly/bootstrap.min.css' rel='stylesheet'>
@@ -16,14 +16,13 @@
             </div>
             <br><br>
             <form action="index.php"> 
-                <label id ="LtoSplit" for="waysToSplit"  >Split how many ways? </label>
+                <label id ="LtoSplit" for="waysToSplit" >Split how many ways? </label>
                 <input type="text" name="waysToSplit" value='<?=sanitize($waysToSplit)?>' required><br>
-                <font id="req">* Required</font>
-                <br>
-                <label id ="Ltab" for="tab"  >How much was the tab? </label>
-                <input type="text" name="tab" value='<?=sanitize($howMuchWasTab)?>' required><br> <font id="req">* Required</font>
-                <br>
-                <label id ="Lserv" for="service"  >How was the service? </label>
+                     <em>* Required</em><br>
+                <label id ="Ltab" for="tab" >How much was the tab? </label>
+                <input type="text" name="tab" value='<?=sanitize($howMuchWasTab)?>' required><br> 
+                     <em>* Required</em><br>
+                <label id ="Lserv" for="service" >How was the service? </label>
                 <select>
                     <option value="exccelent">Excellent</option>
                     <option value="good">Good</option>
@@ -33,14 +32,13 @@
                 <br>
                 <label id ="Lroundup" for="roundup" >Round up?</label>
                 <input id="ck" type='checkbox' name="roundup" <?php if($roundup) echo 'CHECKED' ?>>Yes
-                <br>
-                <br>
+                <br><br>
                 <input type="submit" name="calculate" value="calculate" class='btn btn-primary  btn-small'>
             </form>
 
             <?php if($form->isSubmitted()): ?>
        
-                <div class="alert alert-success">Everyone owes $<?=sanitize(bcdiv((float)$results, 1, 2))?></div>
+                <div class="alert alert-success">Everyone owes $<?=sanitize($results)?></div>
 
             <?php endif; ?> 
         </div>
