07be3c163aa83083199b3949da570d5d1d6cd767
Jody Moore <jodymoore@g.harvard.edu>
2017-02-22 09:53:25 -0600

    update services

--- logic.php
+++ logic.php
@@ -13,12 +13,33 @@ $serviceScale =  '';
 
 $roundup = $form->isChosen('roundup');
 
+$tipAmount = .0;
+
+if(isset($_GET['service']))
+{ 
+	switch($_GET['service'])
+    {
+        case "excellent": $tipAmount = .25; break;
+        case "good": $tipAmount = .20; break;
+        case "ok": $tipAmount = .15; break;
+        case "poor": $tipAmount = .10; break;
+        default: $tipAmount = .15; break;
+    }
+}
+else
+{
+    $tipAmount = .15;
+}
+
+$service = $_GET['service'];
+dump($service);
+
 if ($roundup) {
-	$results = $howMuchWasTab / $waysToSplit; 
+	$results = (($howMuchWasTab * $tipAmount) + $howMuchWasTab) / $waysToSplit; 
 	$results = round($results);
 }
 else {
-	$results = $howMuchWasTab / $waysToSplit; 
+	$results = (($howMuchWasTab * $tipAmount) + $howMuchWasTab) / $waysToSplit; 
 }
 
 $posOfDecimal = strpos($results, '.');
